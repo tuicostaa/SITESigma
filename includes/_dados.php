0b2d2ff92851dccf3fbf649bddbfbf62dc624353
Arthur Costa <tuicosta05@gmail.com>
2022-06-02 12:25:26 -0300

    Cadastro de produtos
    - array de com dados
    - upload de imagens

---
+++ includes/_dados.php
@@ -0,0 +1,14 @@
+<?php
+$produtos = array(
+    0 => array(
+    "nome" => "Capirava Legal",
+    "preço"=> "",
+    "descricao" => "Descrição Capivara Legal",
+    "imagens" => "capivara-legal.jpg")
+)
+
+
+
+
+
+?>
