a429fb5bf959f42e8f0db89ee8d28b4371a93cdb
usuariio <noreply@github.com>
2022-06-09 12:21:04 -0300

    09062022

--- includes/_dados.php
+++ includes/_dados.php
@@ -1,10 +1,25 @@
 <?php
 $produtos = array(
     0 => array(
-    "nome" => "Capirava Legal",
-    "preço"=> "",
-    "descricao" => "Descrição Capivara Legal",
-    "imagens" => "capivara-legal.jpg")
+    "nome" => "Capirava Legal", "preço"=> "", "descricao" => "Descrição Capivara Legal", "imagens" => "capivara-legal.jpg"),
+    1 => array(
+        "nome" => "Suricato", "preço"=> "", "descricao" => "Descrição ", "imagens" => ""),
+        2 => array(
+            "nome" => "Elefante Legal", "preço"=> "", "descricao" => "Descrição ", "imagens" => ""),
+            3 => array(
+                "nome" => "Produto A", "preço"=> "", "descricao" => "Descrição ", "imagens" => ""),
+                4 => array(
+                    "nome" => "Produto B", "preço"=> "", "descricao" => "Descrição ", "imagens" => ""),
+                    5 => array(
+                        "nome" => "Produto C", "preço"=> "", "descricao" => "Descrição ", "imagens" => ""),
+                        6 => array(
+                            "nome" => "Produto D", "preço"=> "", "descricao" => "Descrição ", "imagens" => ""),
+                            7 => array(
+                                "nome" => "Produto E", "preço"=> "", "descricao" => "Descrição ", "imagens" => ""),
+                                8 => array(
+                                    "nome" => "Produto F", "preço"=> "", "descricao" => "Descrição ", "imagens" => ""),
+                                    9 => array(
+                                        "nome" => "Produto 12", "preço"=> "", "descricao" => "Descrição ", "imagens" => "")
 )
 
 
--- index.php
+++ index.php
@@ -3,9 +3,24 @@
 include_once './includes/_dados.php';
 include_once './includes/_head.php';
 include_once './includes/_header.php';
+
 ?>
 
 <h1>Home</h1>
+<?php
+for ($i=0; $i < 4; $i++) { 
+echo '<br>';
+echo $produtos[$i]['nome'];
+echo '<br>';
+echo $produtos[$i]['preço'];
+echo '<img src="./img/' .$produtos[$i]['imagens']. '">';
+echo '<hr>';
+}
+?>
+
+
+
+
 
 <?php
 // include do footer
